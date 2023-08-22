5f8e910caefa9b4f0acb7837fe4fbbfaa69745b0
Tedd OKANO <okano.akifumi@gmail.com>
2023-08-23 03:29:22 +0900

    2 bits toggle experiment. Used faster method to toggle output. Achieved frequency boost from 38kHz to 46kHz

--- bbI2C.py
+++ bbI2C.py
@@ -5,7 +5,8 @@ sda	= machine.Pin( 0, machine.Pin.OUT )
 scl	= machine.Pin( 1, machine.Pin.OUT )
 
 while True:
-	sda.value( 1 )
-	scl.value( 1 )
-	sda.value( 0 )
-	scl.value( 0 )
+	sda.on()
+	scl.on()
+	sda.off()
+	scl.off()
+	
