05c88c9019f3b978adf1898e18ee31f9aa22cd49
Tedd OKANO <okano.akifumi@gmail.com>
2023-08-23 08:42:14 +0900

    i2c write read test

--- bbI2C.py
+++ bbI2C.py
@@ -1,4 +1,5 @@
 from machine import	Pin
+from utime import sleep_ms
 import	utime
 
 class bbI2C:
@@ -12,18 +13,92 @@ class bbI2C:
 		pin.init( Pin.IN )
 		
 		return pin
-
-	def pin_toggle( self ):
+		
+	def start_condition( self ):
 		self.sda.init( Pin.OUT )
 		self.scl.init( Pin.OUT )
-		self.sda.init( Pin.IN )
+
+	def stop_condition( self ):
+		self.sda.init( Pin.OUT )
 		self.scl.init( Pin.IN )
+		self.sda.init( Pin.IN )
+		
+	def send_bytes( self, bytes ):
+		nack	= False
+		
+		for b in bytes:
+			for i in range( 7, -1, -1 ):
+				self.scl.init( Pin.OUT )
+				if (b >> i) & 1:
+					self.sda.init( Pin.IN )
+				else:
+					self.sda.init( Pin.OUT )
+				
+				self.scl.init( Pin.IN )
+			
+			self.scl.init( Pin.OUT )
+			self.sda.init( Pin.IN )
+
+			self.scl.init( Pin.IN )
+			nack	= self.sda.value()
+			self.scl.init( Pin.OUT )
+
+			if nack:
+				return nack
+		
+		return nack
+			
+	def receive_bytes( self, length ):
+		bytes	= []
+		for byte_count in range( length ):
+			b	= 0
+			self.sda.init( Pin.IN )
+
+			# get 8 bits
+
+			for i in range( 7, -1, -1 ):
+				self.scl.init( Pin.OUT )
+				self.scl.init( Pin.IN )
+				b	|= self.sda.value() << i
+				
+			# send ACK/NACK
+
+			self.scl.init( Pin.OUT )
+			
+			if byte_count == (length - 1):
+				self.sda.init( Pin.IN )
+			else:
+				self.sda.init( Pin.OUT )
+						
+			self.scl.init( Pin.IN )
+			self.scl.init( Pin.OUT )
+			
+			bytes	+= [ b ]
+
+		return bytes
+
+	def write( self, addr, data ):
+		data	= [ addr & ~0x01 ] + data
+		self.start_condition()
+		self.send_bytes( data )
+		self.stop_condition()
+		
+	def read( self, addr, length, repeated_start = True ):
+		self.start_condition()
+		self.send_bytes( [ addr | 0x01 ] )
+		data	= self.receive_bytes( length )
+		self.stop_condition()
+		
+		return data
 
 def main():
 	i2c	= bbI2C( 0, 1 )
 
 	while True:
-		i2c.pin_toggle()
-
+		i2c.write( 0x90, [ 0x00 ] )
+		data	= i2c.read( 0x90, 2 )
+		print( f"{data}" )
+		sleep_ms( 100 )
+		
 if __name__ == "__main__":
 	main()
