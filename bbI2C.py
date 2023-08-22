1ebb41b1d4968e71efa3a5b5cc9e2dafd317e2cd
Tedd OKANO <okano.akifumi@gmail.com>
2023-08-23 03:46:32 +0900

    open drain output toggle

--- bbI2C.py
+++ bbI2C.py
@@ -1,12 +1,30 @@
-import	machine
+from machine import	Pin
 import	utime
 
-sda	= machine.Pin( 0, machine.Pin.OUT )
-scl	= machine.Pin( 1, machine.Pin.OUT )
+def main():
+	initialize( 0, 1 )
+	sda	= pin_init( 0 )
+	scl	= pin_init( 1 )
 
-while True:
-	sda.on()
-	scl.on()
-	sda.off()
-	scl.off()
+	while True:
+		sda.init( Pin.OUT )
+		scl.init( Pin.OUT )
+		sda.init( Pin.IN )
+		scl.init( Pin.IN )
+
+
+def initialize( sda_pin, scl_pin ):
+	sda	= pin_init( sda_pin )
+	scl	= pin_init( scl_pin )
+
+
+def pin_init( pin_number ):
+	pin	= Pin( pin_number, Pin.OUT )
+	pin.value( 0 )
+	pin.init( Pin.IN )
 	
+	return pin
+
+
+if __name__ == "__main__":
+	main()
